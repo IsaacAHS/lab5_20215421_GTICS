60e944a25002cadf219ecdb788fab0f7167bc5a5
IsaacAHS <isaac.huamani@pucp.edu.pe>
2024-09-25 19:37:52 -0500

    java

---
+++ LAB5_20215421/src/main/java/com/example/lab5_20215421/Lab520215421Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20215421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520215421Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab520215421Application.class, args);
+	}
+
+}
